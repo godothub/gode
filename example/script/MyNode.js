cb2bb5ed5a53ade5b8312eb90a4cb1aab589bf0d
JustDooooIt <844730658@qq.com>
2026-03-05 14:32:44 +0800

    feat(node): 支持在 res:// 路径下解析 Node.js 模块
    扩展 Node.js 运行时以支持 Godot 资源路径（res://）下的模块解析。修改了 path.join、path.resolve、path.dirname 和 Module._findPath 等核心函数，使其能够正确处理以 'res://' 开头的路径，并添加了在资源目录中查找模块的逻辑（包括处理 package.json 和文件扩展名）。同时取消了示例代码中对 is-odd 模块的注释，以验证功能。

--- example/script/MyNode.js
+++ example/script/MyNode.js
@@ -1,11 +1,11 @@
 const godot = require('godot');
-// const isOdd = require('is-odd');
+const isOdd = require('is-odd');
 
 class MyNode extends godot.Node2D {
 	_ready() {
 		GD.print("ready");
-		// console.log('Is 1 odd?', isOdd(1));
-		// console.log('Is 2 odd?', isOdd(2));
+		console.log('Is 1 odd?', isOdd(1));
+		console.log('Is 2 odd?', isOdd(2));
 	}
 }
 
