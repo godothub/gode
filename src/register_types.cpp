7b6b49ecd7bb44a8ed6dd158582642fbd818302b
moluopro <moluopro@qq.com>
2026-05-07 16:24:06 +0800

    Release script language singletons on shutdown

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -15,6 +15,7 @@
 #include <godot_cpp/classes/resource_saver.hpp>
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/core/memory.hpp>
 #include <godot_cpp/godot.hpp>
 
 void initialize_node_module(godot::ModuleInitializationLevel p_level) {
@@ -44,14 +45,20 @@ void uninitialize_node_module(godot::ModuleInitializationLevel p_level) {
 	if (p_level != godot::MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
-	godot::Engine::get_singleton()->unregister_script_language(gode::JavascriptLanguage::get_singleton());
-	godot::Engine::get_singleton()->unregister_script_language(gode::TypescriptLanguage::get_singleton());
+	gode::JavascriptLanguage *javascript_language = gode::JavascriptLanguage::get_singleton();
+	gode::TypescriptLanguage *typescript_language = gode::TypescriptLanguage::get_singleton();
+
+	godot::Engine::get_singleton()->unregister_script_language(javascript_language);
+	godot::Engine::get_singleton()->unregister_script_language(typescript_language);
 	godot::ResourceSaver::get_singleton()->remove_resource_format_saver(gode::JavascriptSaver::get_singleton());
 	godot::ResourceSaver::get_singleton()->remove_resource_format_saver(gode::TypescriptSaver::get_singleton());
 	godot::ResourceLoader::get_singleton()->remove_resource_format_loader(gode::JavascriptLoader::get_singleton());
 	godot::ResourceLoader::get_singleton()->remove_resource_format_loader(gode::TypescriptLoader::get_singleton());
 
 	gode::NodeRuntime::shutdown();
+
+	memdelete(typescript_language);
+	memdelete(javascript_language);
 }
 
 extern "C" {
